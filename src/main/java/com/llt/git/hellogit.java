c83096dc4c5430eb6e035583b1a899496bfc6961
tom_glb <goodMorning_glb@llt.com>
2020-07-25 21:19:51 +0800

    更新第一次

--- src/main/java/com/llt/git/hellogit.java
+++ src/main/java/com/llt/git/hellogit.java
@@ -3,5 +3,6 @@ package com.llt.git;
 public class hellogit {
     public static void main(String[] args) {
         System.out.println("hhhhhhhhhhhhhhhhhh..git");
+        System.out.println("更新1");
     }
 }
